3de7f21453a3602c31573fb652f072b40140e398
flinder <fridolin.linder@gmail.com>
2015-04-02 23:57:51 -0400

    modified 1_ and made a bit faster + stored output

--- 1_neighborhood_selection.R
+++ 1_neighborhood_selection.R
@@ -1,38 +1,50 @@
-###Testing
+# Find the neighborhoods of counties and of flows, according to
+# specified neighborhood size p
 
-data=read.csv("data/County2010wCoordPOP.csv",header=T)
+library(microbenchmark)
 
-edges=Migration[-which(Migration$dest_geocode==Migration$orig_geocode),]
-edges=edges[,c(18,17)]
-rownames(edges)=seq(nrow(edges))
-pop=data[,2]
-dist=as.matrix(dist(as.matrix(data[,c(3,4)])))
-dist_test=dist(data[,c(3,4)])
+data <- read.csv("data/County2010wCoordPOP.csv", header = TRUE)
+migration <- read.csv("data/countyMigration1978_2011.csv", header = TRUE)
 
+self <- as.integer(migration$dest_geocode) == as.integer(migration$orig_geocode)
+edges <- migration[which(!self & migration$year == 2010), ]
+edges <- edges[ , c(18, 17)]
+rownames(edges) <- seq(nrow(edges))
+pop <- data[,2]
+dist <- as.matrix(dist(as.matrix(data[,c(3,4)])))
 
-n=length(pop)
-p=1000000
-county=list()
-weight=rep(NA,n)
-bandwidth=rep(NA,n)
-names(county)=data[,1]
+n <- length(pop)
+p <- 1000000
+county <- vector(mode = "list", length = n)
+weight <- rep(NA,n)
+bandwidth <- rep(NA,n)
+names(county) <- data[,1]
+
 for(i in 1:n){
-		total=cumsum(pop[order(dist[i,])])
-		which(total>=p)[1]
-		weight[i]=(p-(sum(pop[order(dist[i,])[1:which(total>=p)[1]]]
-			[-length(pop[order(dist[i,])[1:which(total>=p)[1]]])])))/
-			pop[order(dist[i,])[1:which(total>=p)[1]]][length(pop[order(dist[i,])[1:which(total>=p)[1]]])]
-			bandwidth[i]=dist[i,order(dist[i,])[which(total>=p)[1]]]
-			county[[i]]=data[order(dist[i,])[1:which(total>=p)[1]][-1],1]
+    cat(i, "\r")
+    dist_ord <- order(dist[i, ])
+    ord_pop <- pop[dist_ord]
+    total = cumsum(ord_pop)
+    last_c <- which(total>=p)[1]
+    ngbrhd <- dist_ord[1:last_c]
+    pop_n <- pop[ngbrhd]
+    weight[i] <- (p - (sum(pop_n[-length(pop_n)]))) / pop_n[length(pop_n)]  
+    bandwidth[i] <- dist[i, dist_ord[last_c]]
+    county[[i]] <- data[ngbrhd[-1], 1]
 }
 
-flow=list()
-e=length(edges[,1])
-for(i in 1:e){
-	origin=as.character(edges[i,1])
-	dest=as.character(edges[i,2])
-	origin_set=seq(nrow(edges))[!is.na(match(edges[,1],c(county[[origin]],origin)))]
-	dest_set=origin_set[!is.na(match(edges[origin_set,2],c(county[[dest]],dest)))]
-	flow[[i]]=dest_set
-	}
+a <- function(i) {
+    cat(i, "\r")
+    origin <- as.character(edges[i, 1])
+    dest <- as.character(edges[i, 2])
+    orig_n <- c(county[[origin]], origin)
+    dest_n <- c(county[[dest]], dest)
+    origin_set <- which(is.element(edges[, 1], orig_n))
+    dest_set <- origin_set[is.element(edges[origin_set, 2], dest_n)]
+    return(dest_set)
+}
 
+flow <- lapply(seq(1:e), a)
+
+save(
+save(flow, file = "data/flow_neighborhoods.RData")
